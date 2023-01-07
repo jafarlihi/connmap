56901e1fdcef47feb4690faffca70435f489cb90
Hikmat Jafarli <jafarlihi@gmail.com>
2023-01-07 14:48:13 +0400

    Fix compilation in Debian/Ubuntu

--- includes/cairo.h
+++ includes/cairo.h
@@ -2,7 +2,7 @@
 #define CAIRO_CONNMAP_H
 
 #include <cairo.h>
-#include <cairo-xlib.h>
+#include <cairo/cairo-xlib.h>
 #include <stdbool.h>
 
 void draw_world(cairo_surface_t *surface, char *mapName);
